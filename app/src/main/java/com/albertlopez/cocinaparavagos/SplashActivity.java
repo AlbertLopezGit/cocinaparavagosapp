847f330c784ee01f1d1b144c5b262eef23032360
AlbertLopezGit <albertolopezarjona@gmail.com>
2021-04-19 19:31:42 +0200

    solucionado un bug de la pantalla de carga

--- app/src/main/java/com/albertlopez/cocinaparavagos/SplashActivity.java
+++ app/src/main/java/com/albertlopez/cocinaparavagos/SplashActivity.java
@@ -38,9 +38,14 @@ public class SplashActivity extends AppCompatActivity implements Runnable{
 
         //Para esconder la barra de los botones
         View decorView = getWindow().getDecorView();
-        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        int uiOptions = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
+
 
         setContentView(R.layout.activity_splash);
 
