cf57189958a5b7ab277386f64d417183d43e9782
AlbertLopezGit <albertolopezarjona@gmail.com>
2021-05-05 19:04:08 +0200

    Creado el sistema de unidades de los detalles de los ingrediente

--- app/src/main/java/com/albertlopez/cocinaparavagos/IngredientDetailsActivity.java
+++ app/src/main/java/com/albertlopez/cocinaparavagos/IngredientDetailsActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,7 +18,10 @@ public class IngredientDetailsActivity extends AppCompatActivity {
 
     private TextView tvIngrediente,tipoUnidad;
     private ImageView imagenIngrediente;
+    private Button botonMas,botonMenos;
     private Ingredient ingredienteSeleccionado;
+    private TextView numero;
+    private int cantidad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,14 +40,44 @@ public class IngredientDetailsActivity extends AppCompatActivity {
         loadingIngredients();
 
         tvIngrediente = findViewById(R.id.nombreIngrediente);
-        tipoUnidad = tvIngrediente = findViewById(R.id.tipoUnidad);
+        tipoUnidad =  findViewById(R.id.tipoUnidad);
         imagenIngrediente = findViewById(R.id.imagenIngrediente);
+        botonMas = findViewById(R.id.mas);
+        botonMenos = findViewById(R.id.menos);
+        numero = findViewById(R.id.dondeLosNumeros);
 
         String image = ingredienteSeleccionado.getImagen();
         tvIngrediente.setText(ingredienteSeleccionado.getNombreIngrediente());
         tipoUnidad.setText(ingredienteSeleccionado.getValorMedida());
         Picasso.with(this).load(image).into(imagenIngrediente);
 
+        botonMas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              sumar();
+            }
+        });
+
+        botonMenos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                restar();
+            }
+        });
+    }
+
+    private void restar() {
+        cantidad--;
+        if (cantidad >= 0) {
+            numero.setText(String.valueOf(cantidad));
+        } else {
+            cantidad = 0;
+        }
+    }
+
+    private void sumar() {
+        cantidad++;
+        numero.setText(String.valueOf(cantidad));
     }
 
     private void loadingIngredients() {
